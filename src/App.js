8f506ef0c413dda06993b5036eaac6aec298de9b
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-09-17 21:02:14 -0400

    Feature: Created Basic Navbar
    Additions:
     - Basic Navbar that is in dark mode
     - Will have links to bring user to update their profile, log out,
     add a plant, and be in the home page
     - Link to about page
    
    Things to add:
     - We need an app logo or name
     - Different avatar
     - Notification button functionality
     - Highlighted tab on navbar changes when clicked

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import AddPlant from './views/AddPlant';
 import Home from './views/Home';
 import SignIn from './views/SignIn';
 import SignUp from './views/SignUp';
 import UpdateUser from './views/UpdateUser';
+import Navbar from './components/Navbar';
+import AboutUs from './views/AboutUs';
 
 function App() {
   return (
     <div className='App'>
       <Switch>
-        <Route path='/add-plant' component={AddPlant} />{' '}
-        {/*Make Pricate Route*/}
-        <Route path='/home' component={Home} /> {/*Make Pricate Route*/}
-        <Route path='sign-up' component={SignUp} />
-        <Route path='/' component={SignIn} />
+        <Route path='/add-plant'>
+          <Navbar />
+          <AddPlant />
+        </Route>
+
+        <Route path='/update-profile'>
+          <Navbar />
+          <UpdateUser />
+        </Route>
+
+        <Route path='/about'>
+          <Navbar />
+          <AboutUs />
+        </Route>
+
+        <Route path='/home'>
+          <Navbar />
+          <Home />
+        </Route>
+
+        <Route path='sign-up'>
+          <SignUp />
+        </Route>
+
+        <Route path='/'>
+          <SignIn />
+        </Route>
       </Switch>
     </div>
   );
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      {/* Shariq was here */}
       <p>I am the home component</p>
     </div>
   );
