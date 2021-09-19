b59ccffc141200ba98380859880e111256708f4e
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-09-18 20:09:05 -0400

    Added exports to action.js file

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+export const FAILED_REQUEST = 'FAILED_REQUEST';
+export const LOGIN_USER = 'LOGIN_USER';
+export const LOGGED_IN_USER = 'LOGGED_IN_USER';
+
+export const GRAB_PLANTS = 'GRAB_PLANTS';
+export const GOT_PLANTS = 'GOT_PLANTS';
+
+export const ADD_PLANT = 'ADD_PLANT';
+export const ADDED_PLANT = 'ADDED_PLANT';
