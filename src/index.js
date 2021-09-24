2b09f172e07a1cfb0f6f016da6aad94ee85e80e3
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-09-23 20:23:41 -0400

    Bugfix: undid changes in src/index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Router>
     <Provider store={store}>
-      <App />
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
     </Provider>
   </Router>,
   document.getElementById('root')
@@ -22,4 +26,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
